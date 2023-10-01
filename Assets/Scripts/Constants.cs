863a7d375f5bd6281e34d89650162477b80ef6c1
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2023-10-01 15:28:56 +0200

    Decrease money gained per day to 30

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Constants
 {
     public const int MAX_PEOPLE = 100;
-    public const int BASE_MONEY_PER_DAY = 50;
+    public const int BASE_MONEY_PER_DAY = 30;
     public const float BASE_HEALTH_PER_DAY = 0.2f;
     public const float BASE_HAPPYNESS_PER_DAY = -0.2f;
     public const int STARTING_MONEY = 100;
